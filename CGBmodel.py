5a5f38a6c506a55ceddec8e069f23aa885b23d86
Tim Lin <63357025+linzh0205@users.noreply.github.com>
2021-06-09 15:23:01 +0800

    Add files via upload

---
+++ CGBmodel.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+def plot_features(booster, figsize):    
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    return plot_importance(booster=booster, ax=ax)
+
+test  = pd.read_csv('./datasets/test.csv').set_index('ID')
+data = pd.read_pickle('new_train.pkl')
+data = data[[
+    'date_block_num',
+    'shop_id',
+    'item_id',
+    'item_cnt_month',
+    'city_code',
+    'item_category_id',
+    'type_code',
+    'subtype_code',
+    'item_cnt_month_lag_1',
+    'item_cnt_month_lag_2',
+    'item_cnt_month_lag_3',
+    'item_cnt_month_lag_6',
+    # 'item_cnt_month_lag_12',
+    'date_avg_item_cnt_lag_1',
+    'date_item_avg_item_cnt_lag_1',
+    'date_item_avg_item_cnt_lag_2',
+    'date_item_avg_item_cnt_lag_3',
+    'date_item_avg_item_cnt_lag_6',
+    # 'date_item_avg_item_cnt_lag_12',
+    'date_shop_avg_item_cnt_lag_1',
+    'date_shop_avg_item_cnt_lag_2',
+    'date_shop_avg_item_cnt_lag_3',
+    'date_shop_avg_item_cnt_lag_6',
+    # 'date_shop_avg_item_cnt_lag_12',
+    'date_cat_avg_item_cnt_lag_1',
+    'date_shop_cat_avg_item_cnt_lag_1',
+    #'date_shop_type_avg_item_cnt_lag_1',
+    #'date_shop_subtype_avg_item_cnt_lag_1',
+    'date_city_avg_item_cnt_lag_1',
+    'date_item_city_avg_item_cnt_lag_1',
+    #'date_type_avg_item_cnt_lag_1',
+    #'date_subtype_avg_item_cnt_lag_1',
+    'delta_price_lag',
+    'month',
+    'days',
+    'item_shop_last_sale',
+    'item_last_sale',
+    'item_shop_first_sale',
+    'item_first_sale',
+    'item_cnt_month_lag_1_adv',
+    'item_cnt_month_lag_2_adv',
+    'item_cnt_month_lag_3_adv',
+    ]]
+
+X_train = data[data.date_block_num < 33].drop(['item_cnt_month'], axis=1)
+Y_train = data[data.date_block_num < 33]['item_cnt_month']
+X_valid = data[data.date_block_num == 33].drop(['item_cnt_month'], axis=1)
+Y_valid = data[data.date_block_num == 33]['item_cnt_month']
+X_test = data[data.date_block_num == 34].drop(['item_cnt_month'], axis=1)
+
+model = XGBRegressor(
+    max_depth=8,
+    n_estimators=1000,
+    min_child_weight=300, 
+    colsample_bytree=0.8, 
+    subsample=0.8, 
+    eta=0.3,    
+    seed=42)
+
+model.fit(
+    X_train, 
+    Y_train, 
+    eval_metric="rmse", 
+    eval_set=[(X_train, Y_train), (X_valid, Y_valid)], 
+    verbose=True, 
+    early_stopping_rounds = 10)
+
+# save submission
+Y_pred = model.predict(X_valid).clip(0, 20)
+Y_test = model.predict(X_test).clip(0, 20)
+submission = pd.DataFrame({
+    "ID": test.index, 
+    "item_cnt_month": Y_test
+})
+submission.to_csv('submission.csv', index=False)
+# save predictions for an ensemble
+# pickle.dump(Y_pred, open('xgb_train.pickle', 'wb'))
+# pickle.dump(Y_test, open('xgb_test.pickle', 'wb'))
+# plot_features(model, (10,14))
