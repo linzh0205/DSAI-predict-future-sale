dc839668a93924f26ff458b15c0f18cd8e1debbe
anfong-query <71242441+anfong-query@users.noreply.github.com>
2021-06-11 21:01:21 +0800

    Add files via upload

---
+++ LSTMmodel.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from keras.models import Sequential, load_model
+from keras.layers import LSTM, Dense, Activation, ThresholdedReLU, MaxPooling2D, Embedding, Dropout
+from keras.optimizers import Adam, SGD, RMSprop
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler
+import gc
+
+def plot_features(booster, figsize):    
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    return plot_importance(booster=booster, ax=ax)
+
+test  = pd.read_csv('./datasets/test.csv').set_index('ID')
+data = pd.read_pickle('new_train.pkl')
+data = data[[
+    'date_block_num',
+    'shop_id',
+    'item_id',
+    'item_cnt_month',
+    'city_code',
+    'item_category_id',
+    'type_code',
+    'subtype_code',
+    'item_cnt_month_lag_1',
+    'item_cnt_month_lag_2',
+    'item_cnt_month_lag_3',
+    'item_cnt_month_lag_6',
+    # 'item_cnt_month_lag_12',
+    'date_avg_item_cnt_lag_1',
+    'date_item_avg_item_cnt_lag_1',
+    'date_item_avg_item_cnt_lag_2',
+    'date_item_avg_item_cnt_lag_3',
+    'date_item_avg_item_cnt_lag_6',
+    # 'date_item_avg_item_cnt_lag_12',
+    'date_shop_avg_item_cnt_lag_1',
+    'date_shop_avg_item_cnt_lag_2',
+    'date_shop_avg_item_cnt_lag_3',
+    'date_shop_avg_item_cnt_lag_6',
+    # 'date_shop_avg_item_cnt_lag_12',
+    'date_cat_avg_item_cnt_lag_1',
+    'date_shop_cat_avg_item_cnt_lag_1',
+    #'date_shop_type_avg_item_cnt_lag_1',
+    #'date_shop_subtype_avg_item_cnt_lag_1',
+    'date_city_avg_item_cnt_lag_1',
+    'date_item_city_avg_item_cnt_lag_1',
+    #'date_type_avg_item_cnt_lag_1',
+    #'date_subtype_avg_item_cnt_lag_1',
+    'delta_price_lag',
+    'month',
+    'days',
+    'item_shop_last_sale',
+    'item_last_sale',
+    'item_shop_first_sale',
+    'item_first_sale',
+    'item_cnt_month_lag_1_adv',
+    'item_cnt_month_lag_2_adv',
+    'item_cnt_month_lag_3_adv',
+    ]]
+
+X_train = data[data.date_block_num < 33].drop(['item_cnt_month'], axis=1)
+Y_train = data[data.date_block_num < 33]['item_cnt_month']
+X_valid = data[data.date_block_num == 33].drop(['item_cnt_month'], axis=1)
+Y_valid = data[data.date_block_num == 33]['item_cnt_month']
+X_test = data[data.date_block_num == 34].drop(['item_cnt_month'], axis=1)
+
+del data
+gc.collect()
+
+X_train=X_train.values.reshape((X_train.shape[0],X_train.shape[1],1))
+X_valid=X_valid.values.reshape((X_valid.shape[0],X_valid.shape[1],1))
+
+Y_train=Y_train.values.reshape((Y_train.shape[0],1))
+Y_valid=Y_valid.values.reshape((Y_valid.shape[0],1))
+
+X_test=X_test.values.reshape((X_test.shape[0],X_test.shape[1],1))
+
+K.clear_session()
+model_lstm = load_model('model20.h5')
+
+pred = model_lstm.predict(X_test)
+
+submission = pd.DataFrame(pred,columns=['item_cnt_month'])
+submission.to_csv('lstm_submission_1.csv',index_label='ID')
